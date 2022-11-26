09ffbe594c0a5f5c2fe3cfb89c57b42a8099db63
Kamil <kaz4486@gmail.com>
2022-11-26 10:55:08 +0100

    Change server

---
+++ server.mjs
@@ -0,0 +1,32 @@
+// /* global require, process */
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('dist/db/app.json');
+// const middlewares = jsonServer.defaults({
+//   static: 'dist',
+//   noCors: true,
+// });
+
+// const port = process.env.PORT || 3131;
+// server.use(middlewares);
+// server.use(router);
+// server.listen(port);
+
+import path from 'path';
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
+const middlewares = jsonServer.defaults({
+  static: 'dist',
+  noCors: true,
+});
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3131;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
+export default server;
